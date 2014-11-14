f56f167151beb9580bb241ff2e7681ebb073f50d
Kang-min Liu <gugod@gugod.org>
2014-11-14 23:41:28 +0100

    init.

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject cvt "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "CC0 1.0 Universal"
+            :url "http://creativecommons.org/publicdomain/zero/1.0/"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.openimag/faces "1.3.1"]]
+  )
