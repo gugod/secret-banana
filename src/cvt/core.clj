7d1cf9cb0f4a3275e97149ccda746d8a7877d45a
Kang-min Liu <gugod@gugod.org>
2014-11-16 00:32:53 +0100

    copy some functions from cvt.

--- src/cvt/core.clj
+++ src/cvt/core.clj
@@ -1,5 +1,10 @@
 (ns cvt.core)
 
+(defn image-file-seq
+  "file-seq + filter that gives only a sequence of image files."
+  [path]
+  (filter (fn [it] (and (.isFile it) (re-find #".((?i)jpg|png)$" (.getPath it)))) (file-seq (io/file path))))
+
 (defn foo
   "I don't do a whole lot."
   [x]
